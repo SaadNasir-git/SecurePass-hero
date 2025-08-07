f90a904d592fa4010d4c6617eab7c6699f293ca3
SaadNasir-git <mn0871590@gmail.com>
2025-08-07 10:06:06 -0800

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -202,7 +202,7 @@ const PasswordManager = () => {
           )}
           <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
             <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div>
-
+</div>
           <article className="p-3 min-h-[80.7vh] w-full text-center">
             <h1 className="text-4xl font-bold mb-2">
               <span className="text-green-500">&lt;</span>
@@ -454,7 +454,6 @@ const PasswordManager = () => {
             )}
           </article>
         </div>
-      </div>
       </main>
     </>
   );
